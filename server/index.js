a3d428461ebd4ef52c3ea9432ecc24a42d5bffe0
poboll <caiths@icloud.com>
2023-12-26 21:32:13 +0800

    ✨ 增加：连接本地数据库

--- server/index.js
+++ server/index.js
@@ -7,8 +7,11 @@ const app = express();
 // 默认端口，如果环境变量中有指定端口则使用环境变量中的值，否则使用3000端口
 const port = process.env.PORT || 3000;
 
-// 引入名为'admin'的路由模块，并将express实例传递给该模块，这样admin里就有一个app可用
+// 引入后端路由：将express实例传递给该模块，这样admin里就有一个app可用
 require('./routes/admin')(app);
+
+//引入数据库配置
+require('./plugins/db')(app)
 
 // 监听指定端口，当有请求到达时调用回调函数
 app.listen(port, () => {
---
+++ server/plugins/db.js
@@ -0,0 +1,17 @@
+//数据库配置
+// 引入mongoose库，用于连接MongoDB数据库
+const mongoose = require('mongoose');
+
+// 导出一个函数，该函数接受一个Express应用程序实例作为参数
+module.exports = app => {
+
+    // 连接MongoDB数据库，使用'mongodb://127.0.0.1:27017/pvp-site'作为连接字符串
+    mongoose.connect('mongodb://127.0.0.1:27017/pvp-site', {
+        useNewUrlParser: true  // 使用新的URL解析器
+    });
+
+    // 监听数据库连接的一次性事件，一旦数据库连接成功，执行回调函数
+    mongoose.connection.once('open', () => {
+        console.log('连接成功～');  // 在控制台输出连接成功的消息
+    });
+}
