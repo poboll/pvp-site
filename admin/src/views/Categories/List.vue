300fc2f6f1b7e0925f345cb303467c0a2e3d3474
poboll <caiths@icloud.com>
2023-12-28 22:56:41 +0800

    🦄️ 增加：获取分类列表接口

--- admin/src/views/Categories/List.vue
+++ admin/src/views/Categories/List.vue
@@ -1,14 +1,30 @@
 <template>
     <div>
-
-        list
+        <h1>分类列表</h1>
+        <el-table :data="item">
+            <el-table-column prop="_id" label="ID" width="220"></el-table-column>
+            <el-table-column prop="name" label="分类名称"></el-table-column>
+        </el-table>
     </div>
 </template>
-
+  
 <script>
 export default {
-    name: 'list'
-}
+    name: "list",
+    data() {
+        return {
+            list: []
+        };
+    },
+    created() {
+        this.getCategoryList();
+    },
+    methods: {
+        async getCategoryList() {
+            let res = await this.$.get("categories");
+            this.list = res.data;
+        }
+    }
+};
 </script>
-
 <style scoped lang="scss"></style>
