48f45e7af5899046b8b90e8ed75cc01de4bae89f
poboll <caiths@icloud.com>
2024-01-07 22:06:58 +0800

    💨 修改：（文章编辑页面）文章分类选择

--- admin/src/views/Articles/Edit.vue
+++ admin/src/views/Articles/Edit.vue
@@ -4,6 +4,11 @@
         <h1>{{ id ? '编辑' : '新建' }}文章</h1>
         <!-- 表单，@submit.native.prevent="save" 表示在表单提交时调用 save 方法并阻止默认事件 -->
         <el-form label-width="120px" @submit.native.prevent="save">
+            <el-form-item label="所属分类">
+                <el-select v-model="article.categories" multiple>
+                    <el-option v-for="item in categories" :key="item._id" :label="item.name" :value="item._id"></el-option>
+                </el-select>
+            </el-form-item>
             <!-- 文章标题输入框 -->
             <el-form-item label="标题">
                 <el-input v-model="article.title"></el-input>
@@ -27,12 +32,15 @@ export default {
             // 文章信息对象，包含标题
             article: {
                 title: ""
-            }
+            },
+            //文章分类
+            categories: []
         };
     },
     created() {
         // 如果有id属性，则调用getInfo方法获取文章信息
         this.id && this.getInfo();
+        this.getCategories();
     },
     methods: {
         // 获取文章信息
@@ -42,15 +50,20 @@ export default {
             // 将获取到的文章信息保存到article对象中
             this.article = res.data;
         },
+        //获取文章分类
+        async getCategories() {
+            let res = await this.$.get(`rest/categories`);
+            this.categories = res.data;
+        },
         // 保存文章信息，根据id属性选择使用put或post请求
         async save() {
             if (this.id) {
-                await this.$.put(`rest/article/${this.id}`, this.article);
+                await this.$.put(`rest/articles/${this.id}`, this.article);
             } else {
-                await this.$.post("rest/article", this.article);
+                await this.$.post("rest/articles", this.article);
             }
             // 保存成功后跳转到文章列表页
-            this.$router.push("/article/list");
+            this.$router.push("/articles/list");
         }
     }
 };
@@ -65,9 +78,11 @@ export default {
     position: relative;
     overflow: hidden;
 }
+
 .avatar-uploader .el-upload:hover {
     border-color: #409EFF;
 }
+
 .avatar-uploader-icon {
     font-size: 28px;
     color: #8c939d;
@@ -76,6 +91,7 @@ export default {
     line-height: 178px;
     text-align: center;
 }
+
 .avatar {
     width: 178px;
     height: 178px;
