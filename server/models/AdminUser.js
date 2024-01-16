c9745a82055272b8a99c2190a3e3b7a45bee6a6c
poboll <caiths@icloud.com>
2024-01-16 08:38:25 +0800

    💨 修改：（管理员编辑页面）密码以散列方式存储在数据库中

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
     username: { type: String },
-    password: { type: String }
+    password: {
+        type: String,
+        // 查询这个模型的时候不查询password这个字段
+        select: false,
+        // 密码需要加密
+        set(val) {
+            // 使用 bcrypt 库的散列（同步）方法对值进行加密后再存入数据库，加密强度为10
+            return require('bcrypt').hashSync(val, 10)
+        }
+    }
 })
 
 module.exports = mongoose.model('AdminUser', schema)
